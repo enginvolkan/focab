4214c8db18605664a03aee2f89ec670409bf2e84
Engin <engin.volkan@ibm.com>
2019-07-08 14:10:46 -0400

    first commit for backend

---
+++ backend/src/main/java/com/engin/focab/FocabApplication.java
@@ -0,0 +1,13 @@
+package com.engin.focab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocabApplication.class, args);
+	}
+
+}

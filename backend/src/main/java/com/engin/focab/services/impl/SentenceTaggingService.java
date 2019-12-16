24680025a723e4726b66a82387860212d12f7360
Volkan <engin.volkan@thomsonreuters.com>
2019-12-16 11:20:58 -0500

    parameters adjusted according to the windows version

--- backend/src/main/java/com/engin/focab/services/impl/SentenceTaggingService.java
+++ backend/src/main/java/com/engin/focab/services/impl/SentenceTaggingService.java
@@ -1,22 +1,37 @@
 package com.engin.focab.services.impl;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+
+import com.sun.tools.sjavac.Log;
 
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 
 @Component
 public class SentenceTaggingService {
-
+	@Value("${stanford.model.path}")
+	private String modelPath;
+	
 	@Value("classpath:english-left3words-distsim.tagger")
-	private Resource res;
+    private Resource res;
+	
 	private MaxentTagger tagger;
+	private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
 
 	public SentenceTaggingService() {
-		// this.tagger = new MaxentTagger(res.getURI().getPath());
-		this.tagger = new MaxentTagger(
-				"/home/engin/Downloads/stanford-postagger-full-2018-10-16/models/wsj-0-18-left3words-nodistsim.tagger");
+		try {
+			logger.info("Stanford Model Path: " + res.getURI().getRawPath());
+			this.tagger = new MaxentTagger(res.getURI().getRawPath());
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 
 	public String[] tagString(String text) {

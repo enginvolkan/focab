8cba9b7125d180ceb3ec117ebeecc02bca281da1
Engin <engin.volkan@ibm.com>
2020-01-29 23:16:39 -0500

    summaryanalysis

--- backend/src/main/java/com/engin/focab/jpa/SummerizedMovieAnalysisModel.java
+++ backend/src/main/java/com/engin/focab/jpa/SummerizedMovieAnalysisModel.java
@@ -1,17 +1,16 @@
 package com.engin.focab.jpa;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
-import com.engin.focab.jpa.corpus.AggregatedIdiom;
-import com.engin.focab.jpa.corpus.AggregatedPhrasalVerb;
-import com.engin.focab.jpa.corpus.AggregatedSingleWord;
-
 @Entity
-@Table(name = "movieanalysis")
+@Table(name = "summerizedanalysis")
 
 public class SummerizedMovieAnalysisModel {
 
@@ -30,44 +29,52 @@ public class SummerizedMovieAnalysisModel {
 	//////////////
 	// Fields
 	//////////////
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer pk;
 
 	private String imdbId;
 
-	private List<AggregatedIdiom> idioms = new ArrayList<AggregatedIdiom>();
+	private HashMap<String, List<String>> idioms = new HashMap<String, List<String>>();
 
-	private List<AggregatedPhrasalVerb> phrasalVerbs = new ArrayList<AggregatedPhrasalVerb>();
+	private HashMap<String, List<String>> phrasalVerbs = new HashMap<String, List<String>>();
 
-	private List<AggregatedSingleWord> singleWords = new ArrayList<AggregatedSingleWord>();
+	private HashMap<String, List<String>> singleWords = new HashMap<String, List<String>>();
 
 	/////////////////////
 	// Getters and Setters
 	/////////////////////
 
-	public List<AggregatedIdiom> getIdioms() {
-		return idioms;
-	}
-
-	public void setIdioms(List<AggregatedIdiom> idioms) {
-		this.idioms = idioms;
-	}
-
-	public List<AggregatedPhrasalVerb> getPhrasalVerbs() {
-		return phrasalVerbs;
-	}
-
-	public void setPhrasalVerbs(List<AggregatedPhrasalVerb> phrasalVerbs) {
-		this.phrasalVerbs = phrasalVerbs;
-	}
-
-	public List<AggregatedSingleWord> getSingleWords() {
-		return singleWords;
-	}
-
-	public void setSingleWords(List<AggregatedSingleWord> singleWords) {
-		this.singleWords = singleWords;
-	}
-
 	public String getImdbId() {
 		return imdbId;
 	}
+
+	public void setImdbId(String imdbId) {
+		this.imdbId = imdbId;
+	}
+
+	public HashMap<String, List<String>> getIdioms() {
+		return idioms;
+	}
+
+	public void setIdioms(HashMap<String, List<String>> idioms) {
+		this.idioms = idioms;
+	}
+
+	public HashMap<String, List<String>> getPhrasalVerbs() {
+		return phrasalVerbs;
+	}
+
+	public void setPhrasalVerbs(HashMap<String, List<String>> phrasalVerbs) {
+		this.phrasalVerbs = phrasalVerbs;
+	}
+
+	public HashMap<String, List<String>> getSingleWords() {
+		return singleWords;
+	}
+
+	public void setSingleWords(HashMap<String, List<String>> singleWords) {
+		this.singleWords = singleWords;
+	}
+
 }
--- backend/src/main/java/com/engin/focab/services/AnalysisService.java
+++ backend/src/main/java/com/engin/focab/services/AnalysisService.java
@@ -4,16 +4,13 @@ import java.net.MalformedURLException;
 
 import org.apache.xmlrpc.XmlRpcException;
 
-import com.engin.focab.jpa.MovieAnalysisModel;
 import com.engin.focab.jpa.SubtitleModel;
 import com.engin.focab.jpa.SummerizedMovieAnalysisModel;
 
 public interface AnalysisService {
-	public MovieAnalysisModel analyzeMovie(String imdbId) throws MalformedURLException, XmlRpcException;
+	public SummerizedMovieAnalysisModel analyzeMovie(String imdbId) throws MalformedURLException, XmlRpcException;
 
 	public SubtitleModel analyzeSentence(String sentence, boolean analyseIdioms, boolean analysePhrasalVerbs,
 			boolean analyseSingleWords);
-
-	public SummerizedMovieAnalysisModel summerizeMovie(String imdbId);
 
 }

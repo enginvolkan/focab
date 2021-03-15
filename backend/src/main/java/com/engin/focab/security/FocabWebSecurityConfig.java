62cdec7c3f29e4e092f0789f221781fb1c5c9815
enginvolkan <envolkan@gmail.com>
2021-03-14 23:50:20 -0400

    Backend: SSL activated, Logout works

--- backend/src/main/java/com/engin/focab/security/FocabWebSecurityConfig.java
+++ backend/src/main/java/com/engin/focab/security/FocabWebSecurityConfig.java
@@ -43,79 +43,22 @@ public class FocabWebSecurityConfig extends WebSecurityConfigurerAdapter {
 		repository.setCookiePath("/");
 
 		http.cors().and().httpBasic().and().authorizeRequests()
-				.antMatchers("/login", "/home", "/logout"
-				// , "/search", "/analyzeMovie", "/detectIdiom", "/delete"
-				)
+				.antMatchers("/login", "/home")
 				.permitAll()
 				.anyRequest().authenticated()
 				.and().logout()
-				// .permitAll()
-				// .invalidateHttpSession(true).deleteCookies("JSESSIONID")
-				// .logoutSuccessHandler((new
-				// HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK)))
-				.and().csrf().csrfTokenRepository(repository)
-
-				// /logout is handled by Spring security and OPTIONS request fails to pass CORS
-				// check (401). Code below solves this problem.
-				// https://stackoverflow.com/a/36512011/8893760
-//				.and().exceptionHandling().authenticationEntryPoint(new BasicAuthenticationEntryPoint() {
-//					@Override
-//					public void commence(final HttpServletRequest request, final HttpServletResponse response,
-//							final org.springframework.security.core.AuthenticationException authException)
-//							throws IOException, ServletException {
-//						if (HttpMethod.OPTIONS.matches(request.getMethod())) {
-//							response.setStatus(HttpServletResponse.SC_OK);
-//							response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN,
-//									request.getHeader(HttpHeaders.ORIGIN));
-//							response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS,
-//									request.getHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS));
-//							response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS,
-//									request.getHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD));
-//							response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
-//						} else {
-//							response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
-//						}
-//					}
-//				})
-//	         .formLogin()
-////	            .loginPage("/login")
-//	            .permitAll()
-//	            .and()
-//	            .logout()
-//	            .permitAll()
-		;
-//		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
+				.and().csrf().csrfTokenRepository(repository);
 	}
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//		if (initializationParameter == "create" || initializationParameter == "create-drop") {
-//			Customer customer = new Customer("engin");
-//			customer.setEmail("envolkan@gmail.com");
-//			customer.setEnabled(true);
-//			customer.setPassword(passwordEncoder().encode("123qwe"));
-//
-//			Role admin = new Role("ADMIN");
-//			customer.setRoles(new HashSet<Role>());
-//			customer.getRoles().add(admin);
-//
-//			roleRepository.save(admin);
-//			customerRepository.save(customer);
-//		}
 
 		auth.jdbcAuthentication().dataSource(dataSource)
-//		      .withDefaultSchema()
 				.usersByUsernameQuery("select username,password,enabled from customer where username=?")
 				.authoritiesByUsernameQuery(
 						"select customer_username, authority_authority from customer_authority where customer_username=?")
 				.passwordEncoder(passwordEncoder())
-
-		// .withUser(User.withUsername("user")
-//		        .roles("ADMIN"))
 		;
-		// auth
-//	         .inMemoryAuthentication()
-//	         .withUser("user").password("password").roles("USER");
 	}
 
 	@Bean
